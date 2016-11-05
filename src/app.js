e85eaafbdc303a2e57894ef1d0b748819f6bb39b
Gleb <git@gleb.nl>
2016-11-05 16:08:36 +0100

    Added stereo effect for rendering in vr.

--- src/app.js
+++ src/app.js
@@ -1,72 +1,69 @@
-var camera, scene, renderer;
-var geometry, material, mesh;
+var camera, scene, renderer, effect;
+var skyBox;
 
 init();
-animate();
+render();
 
 function init() {
 
-	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
+	camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 5000000 );
 	controls = new THREE.DeviceOrientationControls( camera );
 
 	scene = new THREE.Scene();
 
-	var sides = [
-		{
-			url: 'src/images/posx.jpg',
-			position: [ -512, 0, 0 ],
-			rotation: [ 0, Math.PI / 2, 0 ]
-		},
-		{
-			url: 'src/images/negx.jpg',
-			position: [ 512, 0, 0 ],
-			rotation: [ 0, -Math.PI / 2, 0 ]
-		},
-		{
-			url: 'src/images/posy.jpg',
-			position: [ 0,  512, 0 ],
-			rotation: [ Math.PI / 2, 0, Math.PI ]
-		},
-		{
-			url: 'src/images/negy.jpg',
-			position: [ 0, -512, 0 ],
-			rotation: [ - Math.PI / 2, 0, Math.PI ]
-		},
-		{
-			url: 'src/images/posz.jpg',
-			position: [ 0, 0,  512 ],
-			rotation: [ 0, Math.PI, 0 ]
-		},
-		{
-			url: 'src/images/negz.jpg',
-			position: [ 0, 0, -512 ],
-			rotation: [ 0, 0, 0 ]
-		}
-	];
-
-	var cube = new THREE.Object3D();
-	scene.add( cube );
-
-	for ( var i = 0; i < sides.length; i ++ ) {
-
-		var side = sides[ i ];
-
-		var element = document.createElement( 'img' );
-		element.width = 1026; // 2 pixels extra to close the gap.
-		element.src = side.url;
-
-		var object = new THREE.CSS3DObject( element );
-		object.position.fromArray( side.position );
-		object.rotation.fromArray( side.rotation );
-		cube.add( object );
-
-	}
-
-	renderer = new THREE.CSS3DRenderer();
-	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setClearColor( 0x000000, 0 );
 	document.body.appendChild( renderer.domElement );
 
-	//
+  effect = new THREE.StereoEffect( renderer );
+  effect.setSize( window.innerWidth, window.innerHeight );
+
+  var cubeMap = new THREE.CubeTexture( [] );
+  cubeMap.format = THREE.RGBFormat;
+
+  var loader = new THREE.ImageLoader();
+  loader.load('src/images/skybox.png', function ( image ) {
+
+    var getSide = function ( x, y ) {
+      var size = 1024;
+      var canvas = document.createElement( 'canvas' );
+      canvas.width = size;
+      canvas.height = size;
+      var context = canvas.getContext( '2d' );
+      context.drawImage( image, - x * size, - y * size );
+      return canvas;
+    };
+
+    cubeMap.images[ 0 ] = getSide( 2, 1 ); // px
+    cubeMap.images[ 1 ] = getSide( 0, 1 ); // nx
+    cubeMap.images[ 2 ] = getSide( 1, 0 ); // py
+    cubeMap.images[ 3 ] = getSide( 1, 2 ); // ny
+    cubeMap.images[ 4 ] = getSide( 1, 1 ); // pz
+    cubeMap.images[ 5 ] = getSide( 3, 1 ); // nz
+    cubeMap.needsUpdate = true;
+  });
+
+  var cubeShader = THREE.ShaderLib[ 'cube' ];
+  cubeShader.uniforms[ 'tCube' ].value = cubeMap;
+
+  var skyBoxMaterial = new THREE.ShaderMaterial({
+    fragmentShader: cubeShader.fragmentShader,
+    vertexShader: cubeShader.vertexShader,
+    uniforms: cubeShader.uniforms,
+    side: THREE.BackSide,
+    depthWrite: false,
+  });
+
+  skyBox = new THREE.Mesh(
+    new THREE.BoxGeometry( 1000000, 1000000, 1000000 ),
+    skyBoxMaterial
+  );
+
+  scene.add( skyBox );
+
+  //
 
 	window.addEventListener( 'resize', onWindowResize, false );
 
@@ -77,15 +74,15 @@ function onWindowResize() {
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
 
-	renderer.setSize( window.innerWidth, window.innerHeight );
+	effect.setSize( window.innerWidth, window.innerHeight );
 
 }
 
-function animate() {
+function render() {
 
-	requestAnimationFrame( animate );
+	requestAnimationFrame( render );
 
 	controls.update();
-	renderer.render( scene, camera );
+	effect.render( scene, camera );
 
 }
